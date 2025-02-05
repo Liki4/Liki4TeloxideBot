4c7f34f7c0fd3d136709cbcb8211d94e7cd00741
y01and3 <1791280644@qq.com>
2025-02-05 15:39:21 +0800

    change filter in random to reduce costs

--- src/meme/cmd.rs
+++ src/meme/cmd.rs
@@ -79,14 +79,7 @@ pub async fn handler(
                                 .unwrap()
                                 .iter()
                                 .filter(|(_, info)| {
-                                    (info.params.max_images == 1
-                                        && info.params.min_images == 1
-                                        && info.params.max_texts == 0
-                                        && info.params.min_texts == 0)
-                                        || (info.params.max_texts == 1
-                                            && info.params.min_texts == 1
-                                            && info.params.max_images == 0
-                                            && info.params.min_images == 0)
+                                    info.params.min_images <= 1 && info.params.min_texts <= 1
                                 })
                                 .collect::<Vec<_>>();
                             one_arg_memes.shuffle(&mut rng);
@@ -150,10 +143,7 @@ pub async fn handler(
                                 .unwrap()
                                 .iter()
                                 .filter(|(_, info)| {
-                                    info.params.max_texts == 1
-                                        && info.params.min_texts == 1
-                                        && info.params.max_images == 0
-                                        && info.params.min_images == 0
+                                    info.params.min_images == 0 && info.params.min_texts <= 1
                                 })
                                 .collect::<Vec<_>>();
                             one_arg_memes.shuffle(&mut rng);
